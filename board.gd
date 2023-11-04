c5aa5c3da88f57f0ffe7a6c270f653fa7eae2b71
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-11-04 18:23:48 +0100

    convert to vect

--- board.gd
+++ board.gd
@@ -11,7 +11,7 @@ var grid: Array = [
 
 var selected=-1
 
-var rects: Array
+var cells: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +22,13 @@ func _process(delta):
 	pass
 	
 func found_cell_idx(pt):
-	for i in range(len(rects)):
-		if Rect2(rects[i]).has_point(pt):
+	for i in range(len(cells)):
+		if Rect2(cells[i]).has_point(pt):
 			return i
 	return -1
 
 	
-func _input(event):
-	
+func _input(event):	
 	if Input.is_action_just_released("press"):
 		print("RELEASED")
 		selected=-1
@@ -53,25 +52,24 @@ func _input(event):
 	
 func _draw():
 	var scr = get_viewport().size
-	var width=scr.x/6
-	var height=scr.y/6
-	var midwidth=width/2
-	var midheight=height/2
-	var mindim=min(midheight, midwidth)	
-	rects=[]
+	var cell_size=scr/6
+	var half_cell_size=cell_size/2
+	var mindim = min(half_cell_size.x, half_cell_size.y)
+	cells=[]
 	for i in range(len(grid)):
-		var x = i%6*width
-		var y = i/6*height
-		var radius=mindim/2
+		var pos = Vector2i(i%6,i/6)
+		var scr_pos=pos*cell_size
+		var radius=mindim/1.6
 		if selected==i:
-				radius=radius*1.2
-		rects.append(Rect2(x,y,width,height))		
-		if grid[i]>0:		
-			draw_rect(Rect2(x,y,width,height ),Color.BLANCHED_ALMOND, false, 3)
+			radius=radius*1.4
+		var cell=Rect2(scr_pos,cell_size)
+		cells.append(cell)
+		if grid[i]>0:
+			draw_rect(cell,Color.BLANCHED_ALMOND, false, 3)
 		if grid[i]==2:						
-			draw_circle(Vector2(x+midwidth,y+midheight), radius, Color.RED)
+			draw_circle(scr_pos+half_cell_size, radius, Color.RED)
 		if grid[i]==3:
-			draw_circle(Vector2(x+midwidth,y+midheight), radius, Color.BLUE)
+			draw_circle(scr_pos+half_cell_size, radius, Color.BLUE)
 #	if selected!=-1:		
 #		var x = selected%6*width
 #		var y = selected/6*height
